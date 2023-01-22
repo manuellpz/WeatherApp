8372e2b8210129a2d5993109c302af93bdfc6b16
manuellpz <manuelmzt44@gmail.com>
2023-01-21 18:18:34 -0700

    Funcional

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ const iconCondition = document.querySelector("#icon_condition");
 txtLocation.addEventListener("keyup", (e) => {
   if (e.keyCode === 13) {
     const datos = getInfoWeather(e.target.value);
-    console.log(datos);
+    datos.then((data) => {
+      infoCondition.textContent = data.current.condition.text;
+      infoTemperature.textContent = `${data.current.temp_c}° C`;
+      let icon = data.current.condition.icon.replace(/\d\dx\d\d/g,'128x128');
+      iconCondition.src = icon;
+    });
   }
 });
 
-const getInfoWeather = (location) => {
-  //   const response = await fetch(
-  //     `http://api.weatherapi.com/v1/current.json?key=99711c01514a48c89c600343232001&q=${location}&aqi=yes&lang=es`
-  //   );
-  //   const datos = await response.json();
-  //   return datos;
-  fetch(
-    `http://api.weatherapi.com/v1/current.json?key=99711c01514a48c89c600343232001&q=${location}&aqi=yes&lang=es`
-  )
-    .then((response) => response.json())
-    .then((data) => data);
+const getInfoWeather = async (location) => {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=99711c01514a48c89c600343232001&q=${location}&aqi=no&lang=es`
+  );
+  const datos = await response.json();
+  return datos;
 };
